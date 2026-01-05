12bbd0424e928fde085f5440142ca3d5d6befeb0
brebecek <178239305+brebecek@users.noreply.github.com>
2026-01-05 17:58:32 -0600

    Add logic to end game.
    Add score counter which ends game after one player scores five points. Resets score after someone wins. Outputs to current UI.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,126 +1,108 @@
-function playRound(humanChoice) {
-    let computerChoice = getComputerChoice();
-    const results = document.querySelector("#results");
-    const roundResults = document.createElement("p");
-
-    if (humanChoice === computerChoice) {
-        roundResults.textContent = "You tie!";
-        results.appendChild(roundResults);
-        }
-    
-    else if (humanChoice === "rock") {
-        if (computerChoice === "scissors") {
-            roundResults.textContent = "You win! Rock beats scissors.";
-            results.appendChild(roundResults);
-
-        }
-        else if (computerChoice === "paper") {
-           roundResults.textContent = "You lose! Paper beats rock.";
-           results.appendChild(roundResults);
-        }
-    }
-
-    else if (humanChoice === "paper"){
-        if (computerChoice === "rock"){
-            roundResults.textContent = "You win! Paper beats rock.";
-            results.appendChild(roundResults);
-                }
-        else if (computerChoice === "scissors"){
-            roundResults.textContent = "You lose! Scissors beats paper.";
-            results.appendChild(roundResults);
-                }
-    }
-
-    else if (humanChoice === "scissors"){
-        if (computerChoice === "paper"){
-            roundResults.textContent = "You win! Scissors beats paper.";
-            results.appendChild(roundResults);
-                }
-        else if (computerChoice === "rock"){
-            roundResults.textContent = "You lose! Rocks beats scissors.";
-            results.appendChild(roundResults);
-                }
-    }
-}
 const buttons = document.querySelectorAll("button");
-
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         let humanChoice = button.id;
         playRound(humanChoice);
     });
 });
-
-
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     var computerChoice = Math.floor(Math.random() * 3);
 
     if (computerChoice === 0) {
-        computerChoice = "rock"; 
-    }
-    else if (computerChoice === 1) {
+        computerChoice = "rock";
+    } else if (computerChoice === 1) {
         computerChoice = "paper";
-    }
-
-    else if(computerChoice === 2) {
+    } else if (computerChoice === 2) {
         computerChoice = "scissors";
     }
     return computerChoice;
 }
-/*
-function playGame(){
-    var humanScore = 0;
-    var computerScore = 0;
-    let round = 0;
-    function playRound(humanChoice, ComputerChoice) {
-        //humanChoice = getHumanChoice();//
-        //computerChoice = getComputerChoice();//
-    
+
+function playRound(humanChoice) {
+
+    const results = document.querySelector("#results");
+    const roundResults = document.createElement("p");
+
+        let computerChoice = getComputerChoice();
+
         if (humanChoice === computerChoice) {
-            console.log("You tie!");
-        }
-    
-        else if (humanChoice === "rock") {
+            roundResults.textContent = `You tie! Score is ${humanScore} to ${computerScore}.`;
+            results.appendChild(roundResults);
+        } else if (humanChoice === "rock") {
             if (computerChoice === "scissors") {
-                console.log("You win! Rock beats scissors.")
                 humanScore++;
-            }
-            else if (computerChoice === "paper") {
-                console.log("You lose! Paper beats rock.")
+                if (humanScore == 5){
+                    roundResults.textContent = `${humanChoice} beats ${computerChoice}. You win the game!`;
+                    results.appendChild(roundResults);
+                    humanScore = 0;
+                    computerScore = 0;
+                } else
+                    roundResults.textContent = `You win! ${humanChoice} beats ${computerChoice}. Score is 
+                    ${humanScore} to ${computerScore}.`;
+                    results.appendChild(roundResults);
+
+            } else if (computerChoice === "paper") {
                 computerScore++;
+                if (computerScore == 5){
+                    roundResults.textContent = `${computerChoice} beats ${humanChoice}. You lose the game.`;
+                    results.appendChild(roundResults);
+                    humanScore = 0;
+                    computerScore = 0;
+                } else
+                    roundResults.textContent = `You lose! ${computerChoice} beats ${humanChoice}. Score is 
+                    ${humanScore} to ${computerScore}.`;
+                    results.appendChild(roundResults);
+            }
+        } else if (humanChoice === "paper") {
+            if (computerChoice === "rock") {
+                humanScore++;
+                if (humanScore == 5){
+                    roundResults.textContent = `${humanChoice} beats ${computerChoice}. You win the game!`;
+                    results.appendChild(roundResults);
+                    humanScore = 0;
+                    computerScore = 0;
+                } else
+                    roundResults.textContent = `You win! ${humanChoice} beats ${computerChoice}. Score is 
+                    ${humanScore} to ${computerScore}.`;
+                    results.appendChild(roundResults);
+            } else if (computerChoice === "scissors") {
+                computerScore++;
+                if (computerScore == 5){
+                    roundResults.textContent = `${computerChoice} beats ${humanChoice}. You lose the game.`;
+                    results.appendChild(roundResults);
+                    humanScore = 0;
+                    computerScore = 0;
+                } else
+                    roundResults.textContent = `You lose! ${computerChoice} beats ${humanChoice}. Score is 
+                    ${humanScore} to ${computerScore}.`;
+                    results.appendChild(roundResults);
+            }
+        } else if (humanChoice === "scissors") {
+            if (computerChoice === "paper") {
+                humanScore++;
+                if (humanScore == 5){
+                    roundResults.textContent = `${humanChoice} beats ${computerChoice}. You win the game!`;
+                    results.appendChild(roundResults);
+                    humanScore = 0;
+                    computerScore = 0;
+                } else
+                    roundResults.textContent = `You win! ${humanChoice} beats ${computerChoice}. Score is 
+                    ${humanScore} to ${computerScore}.`;
+                    results.appendChild(roundResults);
+            } else if (computerChoice === "rock") {
+                computerScore++;
+                if (computerScore == 5){
+                    roundResults.textContent = `${computerChoice} beats ${humanChoice}. You lose the game.`;
+                    results.appendChild(roundResults);
+                    humanScore = 0;
+                    computerScore = 0;
+                } else
+                    roundResults.textContent = `You lose! ${computerChoice} beats ${humanChoice}. Score is 
+                    ${humanScore} to ${computerScore}.`;
+                    results.appendChild(roundResults);
             }
         }
-    
-        else if (humanChoice === "paper"){
-            if (computerChoice === "rock"){
-                console.log("You win! Paper beats rock.")
-                humanScore++;
-            }
-            else if (computerChoice === "scissors"){
-                console.log("You lose! Scissors beats rock.")
-                computerScore++;
-            }
-        }
-    
-        else if (humanChoice === "scissors"){
-            if (computerChoice === "paper"){
-                console.log("You win! Scissors beats paper.")
-                humanScore++;
-            }
-            else if (computerChoice === "rock"){
-                console.log("You lose! Rock beats scissors.")
-                computerScore++;
-            }
-        }
-    
     }
-    
-    /*while (round < 5) {
-        playRound();
-        console.log("The score is " + humanScore + " to " + computerScore)
-        round++;
-    }*/
-
-
-//playGame();
