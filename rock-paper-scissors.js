8e9ea4428c838e1e96b39db79da82cb7be421817
brebecek <178239305+brebecek@users.noreply.github.com>
2026-01-08 20:24:14 -0600

    Simply game algorithm in .js file.
    Simplify game algorithm to 58 lines in .js file by using more conditionals per if-else branch, and creating more functions instead of custom text results each time.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,22 +1,30 @@
-const buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        let humanChoice = button.id;
-        playRound(humanChoice);
-    });
-});
-let humanScore = 0;
-let computerScore = 0;
+function gameOverWin(humanChoice, computerChoice) {
+    humanScore = 0;
+    computerScore = 0;
+    return `${humanChoice} beats ${computerChoice}. You win the game!`;
+}
+
+function gameOverLose(humanChoice, computerChoice){
+    humanScore = 0;
+    computerScore = 0;
+    return `${computerChoice} beats ${humanChoice}. You lose the game.`;
+}
+function roundWin(humanChoice, computerChoice, humanScore, computerScore) {
+    return `You win! ${humanChoice} beats ${computerChoice}. Score is ${humanScore} to ${computerScore}.`;
+}
+function roundLose(humanChoice, computerChoice, humanScore, computerScore) {
+    return `You lose! ${computerChoice} beats ${humanChoice}. Score is ${humanScore} to ${computerScore}.`;
+}
 
 function getComputerChoice() {
-    var computerChoice = Math.floor(Math.random() * 3);
+    let computerChoice = Math.floor(Math.random() * 3);
 
     if (computerChoice === 0) {
-        computerChoice = "rock";
+        computerChoice = "Rock";
     } else if (computerChoice === 1) {
-        computerChoice = "paper";
+        computerChoice = "Paper";
     } else if (computerChoice === 2) {
-        computerChoice = "scissors";
+        computerChoice = "Scissors";
     }
     return computerChoice;
 }
@@ -25,84 +33,34 @@ function playRound(humanChoice) {
 
     const results = document.querySelector("#results");
     const roundResults = document.createElement("p");
-
-        let computerChoice = getComputerChoice();
-
-        if (humanChoice === computerChoice) {
-            roundResults.textContent = `You tie! Score is ${humanScore} to ${computerScore}.`;
-            results.appendChild(roundResults);
-        } else if (humanChoice === "rock") {
-            if (computerChoice === "scissors") {
-                humanScore++;
-                if (humanScore == 5){
-                    roundResults.textContent = `${humanChoice} beats ${computerChoice}. You win the game!`;
-                    results.appendChild(roundResults);
-                    humanScore = 0;
-                    computerScore = 0;
-                } else
-                    roundResults.textContent = `You win! ${humanChoice} beats ${computerChoice}. Score is 
-                    ${humanScore} to ${computerScore}.`;
-                    results.appendChild(roundResults);
-
-            } else if (computerChoice === "paper") {
-                computerScore++;
-                if (computerScore == 5){
-                    roundResults.textContent = `${computerChoice} beats ${humanChoice}. You lose the game.`;
-                    results.appendChild(roundResults);
-                    humanScore = 0;
-                    computerScore = 0;
-                } else
-                    roundResults.textContent = `You lose! ${computerChoice} beats ${humanChoice}. Score is 
-                    ${humanScore} to ${computerScore}.`;
-                    results.appendChild(roundResults);
-            }
-        } else if (humanChoice === "paper") {
-            if (computerChoice === "rock") {
-                humanScore++;
-                if (humanScore == 5){
-                    roundResults.textContent = `${humanChoice} beats ${computerChoice}. You win the game!`;
-                    results.appendChild(roundResults);
-                    humanScore = 0;
-                    computerScore = 0;
-                } else
-                    roundResults.textContent = `You win! ${humanChoice} beats ${computerChoice}. Score is 
-                    ${humanScore} to ${computerScore}.`;
-                    results.appendChild(roundResults);
-            } else if (computerChoice === "scissors") {
-                computerScore++;
-                if (computerScore == 5){
-                    roundResults.textContent = `${computerChoice} beats ${humanChoice}. You lose the game.`;
-                    results.appendChild(roundResults);
-                    humanScore = 0;
-                    computerScore = 0;
-                } else
-                    roundResults.textContent = `You lose! ${computerChoice} beats ${humanChoice}. Score is 
-                    ${humanScore} to ${computerScore}.`;
-                    results.appendChild(roundResults);
-            }
-        } else if (humanChoice === "scissors") {
-            if (computerChoice === "paper") {
-                humanScore++;
-                if (humanScore == 5){
-                    roundResults.textContent = `${humanChoice} beats ${computerChoice}. You win the game!`;
-                    results.appendChild(roundResults);
-                    humanScore = 0;
-                    computerScore = 0;
-                } else
-                    roundResults.textContent = `You win! ${humanChoice} beats ${computerChoice}. Score is 
-                    ${humanScore} to ${computerScore}.`;
-                    results.appendChild(roundResults);
-            } else if (computerChoice === "rock") {
-                computerScore++;
-                if (computerScore == 5){
-                    roundResults.textContent = `${computerChoice} beats ${humanChoice}. You lose the game.`;
-                    results.appendChild(roundResults);
-                    humanScore = 0;
-                    computerScore = 0;
-                } else
-                    roundResults.textContent = `You lose! ${computerChoice} beats ${humanChoice}. Score is 
-                    ${humanScore} to ${computerScore}.`;
-                    results.appendChild(roundResults);
-            }
-        }
+    
+    let computerChoice = getComputerChoice();
+    if ((humanChoice == "Rock" && computerChoice == "Scissors") || (humanChoice == "Scissors" && computerChoice == "Paper") || (humanChoice == "Paper" && computerChoice == "Rock")) {
+        humanScore++;
+        if (humanScore < 5) {
+            roundResults.textContent = roundWin(humanChoice, computerChoice, humanScore, computerScore);
+        } else roundResults.textContent = gameOverWin(humanChoice, computerChoice);
+        results.appendChild(roundResults);
+    } else if ((humanChoice == "Rock" && computerChoice == "Paper") || (humanChoice == "Scissors" && computerChoice == "Rock") || (humanChoice == "Paper" && computerChoice == "Scissors")) {
+        computerScore++;
+        if (computerScore < 5) {
+            roundResults.textContent = roundLose(humanChoice, computerChoice, humanScore, computerScore);
+        } else roundResults.textContent = gameOverLose(humanChoice, computerChoice);
+        results.appendChild(roundResults);
+    } else {
+        roundResults.textContent = `You tie! Score is ${humanScore} to ${computerScore}.`;
+        results.appendChild(roundResults);
     }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let humanChoice = button.id;
+        playRound(humanChoice);
+    });
+});
+
